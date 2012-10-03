8924917fe4be72fb909ce887f07dc15a81911a88
Benjamin Gwynn <13gwynn@gmail.com>
2012-10-03 22:38:37 +0100

    Fix device makefile name

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/marvel/device_marvel.mk)
+$(call inherit-product, device/htc/marvel/marvel.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := marvel
