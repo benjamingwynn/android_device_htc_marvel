b20d285f18d165b37f3f4f87e60d0fd001b66d8b
Vignesh Iyer <vigoos@live.com>
2013-05-09 20:18:04 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -5,20 +5,20 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/marvel/marvel.mk)
+$(call inherit-product, device/htc/icong/icong.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := marvel
-PRODUCT_NAME := cm_marvel
+PRODUCT_DEVICE := icong
+PRODUCT_NAME := cm_icong
 PRODUCT_BRAND := htc_europe
-PRODUCT_MODEL := Wildfire S
+PRODUCT_MODEL := Salsa
 PRODUCT_MANUFACTURER := HTC
 
 PRODUCT_VERSION_DEVICE_SPECIFIC := -Build-$(shell date -u +%Y%m%d)
 
 CM_BUILDTYPE := UNOFFICIAL
 
-BUILD_ID := benjamingwynn$(shell date -u +%Y%m%d)
+BUILD_ID := vigoos$(shell date -u +%Y%m%d)
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_marvel BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_marvel/marvel:4.2/$(BUILD_ID)/0.1:user/release-keys PRIVATE_BUILD_DESC="benjamingwynn-cm10.1"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_icong BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_icong/icong:4.2/$(BUILD_ID)/0.1:user/release-keys PRIVATE_BUILD_DESC="vigoos-cm10.1"
