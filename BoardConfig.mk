f3654cdb4e82646c6336a063a184bd41ca5b5261
Vignesh Iyer <vigoos@live.com>
2013-05-09 20:20:45 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_BOOTLOADER_BOARD_NAME := marvel
-TARGET_SPECIFIC_HEADER_PATH := device/htc/marvel/include
+TARGET_BOOTLOADER_BOARD_NAME := icong
+TARGET_SPECIFIC_HEADER_PATH := device/htc/icong/include
 
 ### Board config
 TARGET_NO_BOOTLOADER := true
